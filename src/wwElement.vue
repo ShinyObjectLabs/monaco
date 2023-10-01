92107d5314e4d03b8ba064cc469bb015b04ba4cb
Alphi <114096735+alphi-dev@users.noreply.github.com>
2023-10-01 14:46:08 +0000

    Update package.json and wwElement.vue

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,51 +1,38 @@
 <template>
   <div>
-    <div
-        ref="editorContainer"
-        style="width: 800px; height: 600px; border: 1px solid grey"
-    ></div>
+    <div ref="editorContainer" style="height: 500px;"></div>
   </div>
 </template>
 
 <script>
-import { ref, onMounted, onBeforeUnmount } from 'vue';
-import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
+import * as monaco from 'monaco-editor';
 
 export default {
-  setup() {
-    const editorContainer = ref(null);
-    let editor = null;
-
-    const initMonaco = () => {
-      editor = monaco.editor.create(editorContainer.value, {
-        value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join(
-            '\n'
-        ),
+  data() {
+    return {
+      editor: null,
+    };
+  },
+  mounted() {
+    this.initializeMonacoEditor();
+  },
+  methods: {
+    initializeMonacoEditor() {
+      this.editor = monaco.editor.create(this.$refs.editorContainer, {
+        value: `function x() {
+          console.log("Hello world!");
+        }`,
         language: 'javascript',
         theme: 'vs-dark',
-        minimap: {
-          enabled: false
-        }
       });
-    };
 
-    onMounted(() => {
-      initMonaco();
-    });
-
-    onBeforeUnmount(() => {
-      if (editor) {
-        editor.dispose();
-      }
-    });
-
-    return {
-      editorContainer,
-    };
+      this.editor.onDidChangeModelContent(() => {
+        this.handleEditorChange();
+      });
+    },
+    handleEditorChange() {
+      console.log("Editor content changed:", this.editor.getValue());
+    },
   },
 };
 </script>
-
-<style scoped>
-/* You can add additional styles here if needed */
-</style>
