d236d97a0eb6889beaed08fa5f71dc7a7a99a9bc
Joel Whitaker <112264749+ShinyObjectLabs@users.noreply.github.com>
2023-10-01 16:18:49 +0100

    Revert "Update package.json and wwElement.vue"
    This reverts commit 92107d5314e4d03b8ba064cc469bb015b04ba4cb.

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,38 +1,51 @@
 <template>
   <div>
-    <div ref="editorContainer" style="height: 500px;"></div>
+    <div
+        ref="editorContainer"
+        style="width: 800px; height: 600px; border: 1px solid grey"
+    ></div>
   </div>
 </template>
 
 <script>
-import * as monaco from 'monaco-editor';
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 
 export default {
-  data() {
-    return {
-      editor: null,
-    };
-  },
-  mounted() {
-    this.initializeMonacoEditor();
-  },
-  methods: {
-    initializeMonacoEditor() {
-      this.editor = monaco.editor.create(this.$refs.editorContainer, {
-        value: `function x() {
-          console.log("Hello world!");
-        }`,
+  setup() {
+    const editorContainer = ref(null);
+    let editor = null;
+
+    const initMonaco = () => {
+      editor = monaco.editor.create(editorContainer.value, {
+        value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join(
+            '\n'
+        ),
         language: 'javascript',
         theme: 'vs-dark',
+        minimap: {
+          enabled: false
+        }
       });
+    };
 
-      this.editor.onDidChangeModelContent(() => {
-        this.handleEditorChange();
-      });
-    },
-    handleEditorChange() {
-      console.log("Editor content changed:", this.editor.getValue());
-    },
+    onMounted(() => {
+      initMonaco();
+    });
+
+    onBeforeUnmount(() => {
+      if (editor) {
+        editor.dispose();
+      }
+    });
+
+    return {
+      editorContainer,
+    };
   },
 };
 </script>
+
+<style scoped>
+/* You can add additional styles here if needed */
+</style>
